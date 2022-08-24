27729e818752b33257a1ee8f3f49f11080ff20cb
shauncommits <shaunmbolompo426@gmail.com>
2022-08-24 15:39:50 +0200

    Added the code to ensure that the Hungry Word is able to

--- typingTutor/typingTutor/src/typingTutor/HungryWordMover.java
+++ typingTutor/typingTutor/src/typingTutor/HungryWordMover.java
@@ -2,6 +2,8 @@
 package typingTutor;
 
 /**
+ * HungryWordMover removes the words that are bumped by the Hungry Word
+ * 
  * @author Shaun 
  * Date created 21/08/22
  * @version 1
@@ -13,14 +15,16 @@ public class HungryWordMover extends Thread {
      * Center of attention word is the Hungry Word
      */
     public String target = "Hungry Word"; // use this not simplify the calling of the word's height 
-    static int thislen1,ylen1,ylen1ad,ythislen1; // the height position of the Hungry Word
-    static int thislen2,ylen2,ylen2ad,ythislen2; // the heigth position of the other words 
-    static int bothLen; // the combination of the lenght of the words from thier centers
-    static int diffPos; // the difference in the words x posisition
+    static int lengthSum; // the combination of the words lengths from their centers
+    static int pixelPosition; // the difference in the words pixel positions
+    static int heightDiff; // pixel height difference
     static int size = TypingTutorApp.noWords; // get the size of the array of the words falling words to be used
     static FallingWord[] fall = new FallingWord[size]; // FallingWord array
 	static WordMover[] wordsM = new WordMover[size]; // WordMover array
     static Score score; // The Score object
+    static int thresholdSpeed = 0; // the difference in speed of the words
+    static int threshold;
+     static int halfMaxHeight; // the GUI half size height
 
     /**
      * The HungryWordMover constructor
@@ -32,7 +36,10 @@ public class HungryWordMover extends Thread {
         fall = f;
         wordsM = w;
         score = sco;
+        halfMaxHeight = fall[0].getMaxHeight()/2; // get half of the GUI height
     }
+
+
 	
 	public synchronized void run() {
 		while(true){ // Execution that is executed while the Main Thread has not ended
@@ -41,31 +48,51 @@ public class HungryWordMover extends Thread {
                     if(i==j)
                         continue;
                             if(fall[i].getWord().equals("Hungry Word")){ // code to be implement when the Hungry Word is on the screen
-                                // Get the surrrounding y values of the Hungry Word in the screen as it is moving
-                                ylen1 = -8 + fall[i].getY();
-                                ylen1ad = ylen1-14;
-                                ylen2 = 8 + fall[i].getY();
 
-                                // Get the surrrounding y values of the other words in the screen as they are moving
-                                ythislen1 = -8 + fall[j].getY();
-                                ylen2ad = ylen2 + 14;
-                                ythislen2 = 8 + fall[j].getY();
+                                if(fall[i].getY()>50){
+                                    heightDiff = Math.abs(fall[i].getY()-fall[j].getY());
+                                    thresholdSpeed = Math.abs(fall[i].getSpeed()-fall[j].getSpeed());
+                                    if(fall[i].getY()<halfMaxHeight){
+                                    if(thresholdSpeed<200)
+                                        threshold = 15;
+                                    else if(thresholdSpeed>199&&thresholdSpeed<400)
+                                        threshold = 25;
+                                    else if(thresholdSpeed>399&&thresholdSpeed<600)
+                                        threshold = 35;
+                                        else if(thresholdSpeed>599&&thresholdSpeed<900)
+                                        threshold = 45;
+                                    else
+                                        threshold = 55;
+                                    } 
+                                    else{
+                                        if(fall[j].getSpeed()<200)
+                                        threshold = 66;
+                                    else if(fall[j].getSpeed()>199&&fall[j].getSpeed()<400)
+                                        threshold = 56;
+                                    else if(fall[j].getSpeed()>399&&fall[j].getSpeed()<600)
+                                        threshold = 46;
+                                        else if(fall[j].getSpeed()>599&&fall[j].getSpeed()<900)
+                                        threshold = 36;
+                                    else
+                                        threshold = 26;
+                                    } 
+                                      
+                                    if(heightDiff<threshold){  // if(fall[i].getSpeed())
+                                        lengthSum = fall[i].wordPixelLenght()+fall[j].wordPixelLenght();
+                                        pixelPosition = Math.abs(fall[i].getX()-fall[j].getX());
+                                        if(pixelPosition<lengthSum){
+                                            fall[j].resetWord();
+                                            score.missedWord();
+                                            System.out.println("Diff "+pixelPosition+" both "+lengthSum);
+                                            pixelPosition = 10000;
+                                            lengthSum = 10000;
+                                        }
+                                    }
+                                }
 
                                 if(fall[i].getX()>940){ // When the Hungry Word not scored and manages to go out screen the word is reseted and number of missed words are incremented
                                     fall[i].resetWord();
                                     score.missedWord();
-                                }
-                                if(fall[i].getY()>45){ // The Hungry Word start to eat other words when it is visible on the screen
-                                    
-                                    if((ythislen1>=ylen2&&ythislen1>=ylen2ad)||(ythislen2>=ylen1&&ythislen2>=ylen1ad)){ // Check if the y values of the Hungry Word and the values of other word are overlapping 
-                                        bothLen = (target.length()+fall[j].getWord().length())/2; // Add the lengths of both half the hungry word and other words
-                                        diffPos = Math.abs(fall[i].getX()-fall[j].getX()); // get the difference between the x postions of the words that are almost overlap in y values with the Hungry Word
-                                        if(bothLen>=diffPos){ // check if the difference in the x values of the words is less than the sum of their half lengths and if so the words are touching eac other
-                                            fall[j].resetWord();
-                                            score.missedWord();
-                                            bothLen = 1000;
-                                        }
-                                    } 
                                 }
                             } 
                 }	 
